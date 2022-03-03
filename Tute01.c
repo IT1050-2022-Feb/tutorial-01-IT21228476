d00837be4dc0f9a0213b577fe5ad8296808001a3
KAVI_X <100861963+IT21228476@users.noreply.github.com>
2022-03-03 16:47:58 +0000

    completed....

--- Tute01.c
+++ Tute01.c
@@ -1,11 +1,24 @@
-/* Exercise 1 - Calculations
 
-   Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
 
 #include <stdio.h>
 
 int main() {
-  
+
+  int m1,m2;
+  float avg;
+
+  printf("Enter the subject 1 mark:");
+  scanf("%d",&m1);
+
+  printf("Enter the subject 2 mark:");
+  scanf("%d",&m2);
+
+  //calculation
+
+  avg=(m1+m2)/2;
+
+  printf("Average mark of two subjects :%.2f",avg);
+
   return 0;
 }
-
+     
--- Tute03.c
+++ Tute03.c
@@ -1,19 +1,22 @@
-/*Exercise 3 - Repetition
 
-Write a C program to calculate the sum of the numbers from 1 to n.
-Where n is a keyboard input.
-
-e.g.
-
-n -> 100
-sum = 1+2+3+....+ 99+100 = 5050
-
-n -> 1-
-sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
-int main() {
-  
+  int main() {
+
+
+  int number , total = 0 , x;
+
+  printf("Enter a number :");
+  scanf("%d",&number);
+
+  for(x = 1 ; x <= number ; x++)
+  {
+     total = total + x;  
+
+  }
+
+  printf("total is %d",total);
+
   return 0;
 }
 
